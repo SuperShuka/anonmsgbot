9cc3587db42a72504b52fc9def4b1b48553416a3
Sanya <57352066+SuperShuka@users.noreply.github.com>
2024-04-30 14:55:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 load_dotenv(find_dotenv())
 
-BOT_TOKEN = os.environ.get('TOKEN', None)
-BOT_USERNAME = ''  # https://t.me/BOTUSERNAME Нужно для генерации реферальной ссылки
+BOT_TOKEN = ""
+BOT_USERNAME = ""  # https://t.me/BOTUSERNAME Нужно для генерации реферальной ссылки
 otstuk_chat = -732422972
 ADMINS_ID = [1165126773]  # можно добавить много через запятую. Пример  [798661023, 798661023, 798661023]
 # DATABASE
